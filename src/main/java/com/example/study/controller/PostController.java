766b369be81393ebd92833fb5f1b347868286576
chjin <seedevk8s@gmail.com>
2019-12-03 23:12:29 +0900

    post 처리 테스트

---
+++ src/main/java/com/example/study/controller/PostController.java
@@ -0,0 +1,21 @@
+package com.example.study.controller;
+
+import com.example.study.model.SearchParam;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class PostController {
+
+    // HTML <Form>
+    // ajax 검색
+    // http post body -> data
+    // json, xml, multipart-form / text-plain
+
+
+   // @RequestMapping(method = RequestMethod.POST, path = "/postMethod")
+    @PostMapping(value = "/postMethod"  /*, produces = {"application-json"}*/)
+    public SearchParam postMethod(@RequestBody SearchParam searchParam){
+        return searchParam;
+    }
+}
