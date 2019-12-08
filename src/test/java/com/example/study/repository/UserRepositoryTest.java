2c4411fb852f3e4d8e65c44a264c123410d276e2
chjin <seedevk8s@gmail.com>
2019-12-08 22:13:39 +0900

    DAO crud 기능 태스트 클래스 추가

---
+++ src/test/java/com/example/study/repository/UserRepositoryTest.java
@@ -0,0 +1,22 @@
+package com.example.study.repository;
+
+import com.example.study.StudyApplicationTests;
+
+public class UserRepositoryTest extends StudyApplicationTests {
+
+    public void create(){
+
+    }
+
+    public void read(){
+
+    }
+
+    public void update(){
+
+    }
+
+    public void delete(){
+
+    }
+}
