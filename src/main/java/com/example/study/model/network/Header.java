ac40ab3b317324e319d52fc4adef8444688115ea
chjin <seedevk8s@gmail.com>
2019-12-15 13:53:56 +0900

    네트워크상 주고받는거 확인하기

---
+++ src/main/java/com/example/study/model/network/Header.java
@@ -0,0 +1,20 @@
+package com.example.study.model.network;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Header {
+
+    // api 통신 시간
+
+    // api 응답 코드
+
+    // api 부가 설명
+
+}
