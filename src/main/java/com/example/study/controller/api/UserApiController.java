1985498da515d2ef11a0be80d0e08a8287be2d1b
chjin <seedevk8s@gmail.com>
2019-12-15 21:29:26 +0900

    사용자 API CREATE 기능 추가

--- src/main/java/com/example/study/controller/api/UserApiController.java
+++ src/main/java/com/example/study/controller/api/UserApiController.java
@@ -4,17 +4,26 @@ import com.example.study.ifs.CrudInterface;
 import com.example.study.model.network.Header;
 import com.example.study.model.network.request.UserApiRequest;
 import com.example.study.model.network.response.UserApiResponse;
+import com.example.study.service.UserApiLogicService;
+import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/user")
 public class UserApiController implements CrudInterface<UserApiRequest, UserApiResponse> {
 
+    @Autowired
+    private UserApiLogicService userApiLogicService;
 
     @Override
     @PostMapping("")        //api/user
-    public Header<UserApiResponse> create(@RequestBody UserApiRequest userApiRequest) {
-        return null;
+    public Header<UserApiResponse> create(@RequestBody Header<UserApiRequest> request) {
+        //System.out.println();
+        log.info("{} ",request,"ABC");  // request.toString()
+        return userApiLogicService.create(request);
     }
 
     @Override
@@ -25,7 +34,7 @@ public class UserApiController implements CrudInterface<UserApiRequest, UserApiR
 
     @Override
     @PutMapping("")         //api/user
-    public Header<UserApiResponse> update(@RequestBody UserApiRequest request) {
+    public Header<UserApiResponse> update(@RequestBody Header<UserApiRequest> request) {
         return null;
     }
 
--- src/main/java/com/example/study/model/entity/User.java
+++ src/main/java/com/example/study/model/entity/User.java
@@ -1,8 +1,11 @@
 package com.example.study.model.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import com.example.study.model.enumclass.UserStatus;
+import lombok.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -11,18 +14,41 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor   // 기본생성자
 @Entity // == table
+@ToString(exclude = {"orderGroupList"})
+@Builder
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String account;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private UserStatus status;      //REGISTERED / UNREGISTERED / WAITING
+
     private String email;
+
     private String phoneNumber;
+
+    private  LocalDateTime registeredAt;
+
+    private  LocalDateTime unregisteredAt;
+
+    @CreatedDate
     private LocalDateTime createdAt;
+
+    @CreatedBy
     private String createdBy;
+
+    @LastModifiedDate
     private LocalDateTime updatedAt;
+
+    @LastModifiedBy
     private String updatedBy;
+
 
 
 }
